828146fc6553d7b9171fd8f2d07ad850d4dfcb9e
AbdulMoeedkh <makhansherwanii@gmail.com>
2025-03-24 00:54:26 -0700

    connected with mongo-db atlas

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -3,7 +3,7 @@ import serverConfig from "./serverConfig.js";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/newDB"); 
+        await mongoose.connect("mongodb+srv://amk-21:A03447732310k@fyp.wrcaf.mongodb.net/"); 
         // Console.log(serverConfig.dbURI) 
     } catch (error) {
         console.error("Error ==>", error.message);
